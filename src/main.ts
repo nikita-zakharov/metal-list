5167e7311c3631e34a0eb5fda55acce5d1d3bda7
Nikita Zakharov <nikita.zakharov@dsr-corporation.com>
2022-10-26 21:28:59 +0300

    feat: add success, error messages, renames

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,24 @@
 import $ from "jquery";
 import "./style.css";
 
-const form = document.getElementById("form-message") as HTMLFormElement;
+const form = document.getElementById("form") as HTMLFormElement;
 
 function submitForm(e: SubmitEvent) {
   e.preventDefault();
   const isValid = form.checkValidity();
   form.reportValidity();
   if (!isValid) return;
-
   $.ajax({
     type: "POST",
     url: "send.php",
     data: $(form).serialize(),
     success: function () {
-      $("#contactSuccess").fadeIn().delay(5000).fadeOut();
+      $("#submitSuccessMessage").fadeIn().delay(5000).fadeOut();
     },
     error: function () {
-      $("#contactError").fadeIn().delay(5000).fadeOut();
+      $("#submitErrorMessage").fadeIn().delay(5000).fadeOut();
     },
   });
 }
-form.onsubmit = submitForm;
+
+form.addEventListener("submit", submitForm);
