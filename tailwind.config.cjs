6b1735cea47c344526bf79e333d232f7e593bfdf
Nikita Zakharov <nikita.zakharov@dsr-corporation.com>
2022-10-26 20:28:19 +0300

    init commit

---
+++ tailwind.config.cjs
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  theme: {
+    fontFamily: {
+      sans: [
+        "Open Sans, sans-serif",
+      ],
+    },
+    container: {
+      screens: {
+        sm: '600px',
+        md: '750px',
+        lg: '970px',
+        xl: '1170px',
+        '2xl': '1170px',
+      },
+    },
+    extend: {
+    },
+  },
+  plugins: [],
+};
