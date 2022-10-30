53af53a1cf9dbdccff2cfbc6142f740b8e1ce197
Nikita Zakharov <nikita.zakharov@dsr-corporation.com>
2022-10-30 14:56:15 +0300

    add prettierignore file, format files

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,21 +3,18 @@ module.exports = {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
     fontFamily: {
-      sans: [
-        "Open Sans, sans-serif",
-      ],
+      sans: ["Open Sans, sans-serif"],
     },
     container: {
       screens: {
-        sm: '600px',
-        md: '750px',
-        lg: '970px',
-        xl: '1170px',
-        '2xl': '1170px',
+        sm: "600px",
+        md: "750px",
+        lg: "970px",
+        xl: "1170px",
+        "2xl": "1170px",
       },
     },
-    extend: {
-    },
+    extend: {},
   },
   plugins: [],
 };
