f5aacbe03584082c3a8ba0b4da5561707636ac12
NikitaZakharov <nikzhrv@yandex.ru>
2024-06-19 09:25:24 +0300

    add policy, cookie toast

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,34 @@ import { appendGoogleMap } from "./scripts/google-map";
 
 import "./style.css";
 
+const cookieModal = document.querySelector('#cookieModal')
+const closeCookieButton = document.querySelector('#closeCookieButton')
+const applyCookieButton = document.querySelector('#applyCookieButton')
+
+const setReadCookie = () => {
+  localStorage.setItem('cookie-applied', 'true')
+}
+
+const checkReadCookie = () => {
+  return Boolean(localStorage.getItem('cookie-applied'))
+}
+
+const showCookiePolicyToast = () => {
+  if (checkReadCookie()) return
+  cookieModal?.classList.remove('hidden')
+}
+
+applyCookieButton?.addEventListener('click', () => {
+  cookieModal?.classList.add('hidden')
+  setReadCookie()
+})
+
+
+closeCookieButton?.addEventListener('click', () => {
+  cookieModal?.classList.add('hidden')
+  setReadCookie()
+})
+
 function setCurrentYear() {
   const currentYear = new Date().getFullYear();
   const el = document.getElementById("year") as HTMLSpanElement;
@@ -12,6 +40,7 @@ function setCurrentYear() {
 }
 
 document.addEventListener("DOMContentLoaded", () => {
+  showCookiePolicyToast()
   setCurrentYear();
   appendGoogleMap();
 });
